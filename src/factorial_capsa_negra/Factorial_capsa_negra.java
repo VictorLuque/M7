dc58fa503d1a85fab45e5dc4cfb8cfe3ce4a042d
cfgs <cfgs@pc-info-host>
2017-04-25 19:39:14 +0200

    Afegits dos mètodes

--- src/factorial_capsa_negra/Factorial_capsa_negra.java
+++ src/factorial_capsa_negra/Factorial_capsa_negra.java
@@ -12,4 +12,16 @@ public class Factorial_capsa_negra {
     public static void main(String[] args){
         System.out.println(factorial(Integer.parseInt(args[0])));
     }
+    private static int factorial2(int n){
+        if(n == 0 || n == 1){
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+    private static int prova(int n){
+        if(n == 0 || n == 1){
+            return 1;
+        }
+        return n * prova(n - 1);
+    }
 }
